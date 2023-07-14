5af1e485b30735214e09c47ef862039ffb760bea
phamphuocnghia21082002 <100588926+Pham-Phuoc-Nghia@users.noreply.github.com>
2023-07-14 08:06:56 +0700

    first commit

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'clustersgp.6b8bcmz.mongodb.net',
+    DB_USER: 'vincent',
+    DB_PASS: '000000nghia',
+    DB_DATABASE: 'shoppingonline',
+    EMAIL_USER: '<phamphuocnghia21082002@gmail.com>',
+    EMAIL_PASS: '<fmwkuokajylmquqq>',
+    JWT_SECRET: 'celica12345',
+    JWT_EXPIRES: '3600000', // in milliseconds
+  };
+  module.exports = MyConstants;
